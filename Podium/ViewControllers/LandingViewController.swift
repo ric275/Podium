a6f9f09b8db611a25673e9b3da07f22297a4db35
Jack Taylor <ajacktaylor@me.com>
2018-04-07 18:31:31 +0100

    Scroll view
    Added welcome scroll and first image,

--- Podium/ViewControllers/LandingViewController.swift
+++ Podium/ViewControllers/LandingViewController.swift
@@ -12,8 +12,28 @@ class LandingViewController: UIViewController {
     
     @IBOutlet weak var versionLabel: UILabel!
     
+    @IBOutlet weak var imageScroll: UIScrollView!
+    
+    var images = [UIImage]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+                
+        images = [#imageLiteral(resourceName: "Podium splash 1"),#imageLiteral(resourceName: "paramore"),#imageLiteral(resourceName: "gameshow"),#imageLiteral(resourceName: "in our bones"),#imageLiteral(resourceName: "true colours"),#imageLiteral(resourceName: "oh my my")]
+        
+        for i in 0..<images.count {
+            
+            let imageView = UIImageView()
+            imageView.image = images[i]
+            imageView.contentMode = .scaleAspectFit
+            let xPosition = self.view.frame.width * CGFloat(i)
+            
+            imageView.frame = CGRect(x: xPosition, y: 0, width: self.imageScroll.frame.width, height: self.imageScroll.frame.height)
+            
+            imageScroll.contentSize.width = imageScroll.frame.width * CGFloat(i + 1)
+            
+            imageScroll.addSubview(imageView)
+        }
         
         if let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String {
             if let build = Bundle.main.infoDictionary?["CFBundleVersion"] as? String {
